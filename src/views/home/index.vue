33d5a18e26d9f33815023a00988b63e717edc630
Mitchell Gritts <mitchellgritts@pm.me>
2021-12-10 10:46:42 -0800

    UI Testing: h-screen on mobile

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class='flex-1 relative flex items-center justify-center overflow-hidden bg-gray-800'>
+  <div class="flex flex-col items-center justify-between bg-blue-300 w-full">
+    <div class="flex items-center content-center bg-red-300 w-36 h-36">TOP</div>
+    <div class="flex items-center content-center w-36 h-36">
+      <router-link
+          to="/map"
+          class="inline-block px-4 py-2 rounded-md text-xl text-gray-100 bg-olive-500 border border-olive-500 shadow-md hover:bg-olive-600"
+        >
+          Explore The Map
+        </router-link>
+    </div>
+    <div class="flex items-center content-center bg-red-300 w-36 h-36">BOTTOM</div>
+  </div>
+  <!-- <div class='flex-1 relative flex items-center justify-center overflow-hidden bg-gray-800'>
     <header class="relative z-30 w-full mx-4 lg:mx-0 lg:w-2/3 xl:w-1/2">
       <h3 class="text-shadow mb-2 ml-2 font-mono text-3xl font-light tracking-widest text-gray-300">FishNV</h3>
       <h2 class="text-shadow text-8xl font-extrabold tracking-wider text-olive-400">Get Online</h2>
@@ -24,7 +36,6 @@
       </div>
     </header>
 
-    <!-- image -->
     <img
       src="https://ndow-cdn.s3-us-west-2.amazonaws.com/species/brook-trout/brook-trout-hero.jpg"
       alt="A brook trout laying in the grass with a fly fishing pole and reel above the fish."
@@ -32,7 +43,7 @@
     >
     <div class="absolute h-full w-full object-cover bg-gray-600 vignette"></div>
     <div class="absolute h-full w-full object-cover opacity-90 bg-gray-600 mix-blend-overlay"></div>
-  </div>
+  </div> -->
 </template>
 
 
