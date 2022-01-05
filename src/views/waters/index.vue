1d9488a1c6a110059f8d14deac7d0eb737052b6a
Mitchell Gritts <mitchellgritts@pm.me>
2022-01-05 13:55:34 -0800

    Fixed: relative position for map menu button parent

--- src/views/waters/index.vue
+++ src/views/waters/index.vue
@@ -191,7 +191,9 @@
         </header>
 
         <main class="rounded border flex-grow mt-8 bg-gray-50 rounded-t-lg shadow space-y-6">
-          <div class="w-full h-96 lg:h-156 rounded-t-lg overflow-hidden border-b border-gray-300">
+          <div
+            class="relative w-full h-96 lg:h-156 rounded-t-lg overflow-hidden border-b border-gray-300"
+          >
             <maplibre-map />
           </div>
 
